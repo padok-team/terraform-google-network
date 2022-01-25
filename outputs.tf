477aaccd9775d0ffe60418238e29ff48bfc65380
Nicolas Levée <nlevee@users.noreply.github.com>
2022-01-25 23:12:26 +0100

    fix(outputs.tf): change output name for vpc_access_connector

--- outputs.tf
+++ outputs.tf
@@ -7,7 +7,7 @@ output "compute_network" {
   description = "Name of created network and its subnets."
 }
 
-output "cloudrun_access_connector_name" {
-  value       = try(google_vpc_access_connector.this[0].name, "")
+output "vpc_access_connector_name" {
+  value       = one(google_vpc_access_connector.this[*].name)
   description = "Name of created access connector (filled if 'cloudrun=true')"
 }

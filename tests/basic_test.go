394107e2de843c77532381d2024cb31f69e6c85c
Thomas de Lachaux <thomasd@padok.fr>
2023-09-22 16:39:07 +0200

    feat: add testing

---
+++ tests/basic_test.go
@@ -0,0 +1,39 @@
+package test
+
+import (
+	"testing"
+  "flag"
+
+	"github.com/gruntwork-io/terratest/modules/terraform"
+	"github.com/stretchr/testify/assert"
+)
+
+
+var destroy = flag.Bool("destroy", false, "destroy the infrastructure after testing")
+
+func TestTerraformHelloWorldExample(t *testing.T) {
+	// retryable errors in terraform testing.
+	terraformOptions := terraform.WithDefaultRetryableErrors(t, &terraform.Options{
+		TerraformDir: "..",
+    VarFiles: []string{"tests/tests.tfvars"},
+	})
+
+
+
+  if *destroy {
+    defer terraform.Destroy(t, terraformOptions)
+  }
+
+	// defer terraform.Destroy(t, terraformOptions)
+
+	terraform.InitAndApply(t, terraformOptions)
+
+	network_id := terraform.Output(t, terraformOptions, "network_id")
+	assert.Equal(t, "projects/padok-library-gcp-host/global/networks/testing", network_id)
+
+  network_name := terraform.Output(t, terraformOptions, "network_name")
+	assert.Equal(t, "testing", network_name)
+
+  network_self_link := terraform.Output(t, terraformOptions, "network_self_link")
+	assert.Equal(t, "https://www.googleapis.com/compute/v1/projects/padok-library-gcp-host/global/networks/testing", network_self_link)
+}
---
+++ tests/tests.tfvars
@@ -0,0 +1,21 @@
+  name       = "testing"
+  project_id = "padok-library-gcp-host"
+
+  subnets = {
+
+    "eu-1" = {
+      name             = "eu-1"
+      region           = "europe-west1"
+      primary_cidr     = "172.16.0.0/20"
+      serverless_cidr  = ""
+      secondary_ranges = {}
+    },
+    "eu-2" = {
+      name             = "eu-2"
+      region           = "europe-west2"
+      primary_cidr     = "172.16.48.0/20"
+      serverless_cidr  = "172.16.80.0/28"
+      secondary_ranges = {}
+    }
+  }
+  gcp_peering_cidr = "172.16.64.0/20"
